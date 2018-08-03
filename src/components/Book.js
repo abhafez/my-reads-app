057bbb2972edb539d623c9b7d70089392437639a
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-08-03 02:19:37 +0200

    Split hard coded ui to components

---
+++ src/components/Book.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as BooksAPI from '../BooksAPI'
+
+class Book extends React.Component {
+  state = {
+    books: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(edit:)' }}></div>
+          <div className="book-shelf-changer">
+            <select>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">edit:</div>
+        <div className="book-authors">edit:</div>
+      </div>
+    );
+  }
+}
+
+export default Book;
