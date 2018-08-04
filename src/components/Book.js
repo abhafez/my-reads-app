67daed40174ac37fe221b3d734d241bfc59e0ff1
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-08-04 21:47:10 +0200

    Make select shelf change but with a refresh

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,25 +1,36 @@
 import React from 'react'
-
-
-
+import * as BooksAPI from '../BooksAPI'
 class Book extends React.Component {
-  state = {
-    currentShelf: this.props.bookDetails.shelf,
-    newShelf: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.bookDetails.shelf,
+      bookID: this.props.bookDetails.id
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({value: event.target.value, bookID: this.props.bookDetails.id});
+    BooksAPI.update(this.props.bookDetails, event.target.value)
+  }
+
   render() {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192,
-            backgroundImage: `url(${this.props.bookDetails.imageLinks.smallThumbnail})`}}></div>
+          <div className="book-cover" style={{
+            width: 128, height: 192,
+            backgroundImage:
+            `url(${this.props.bookDetails.imageLinks.smallThumbnail})`
+          }}></div>
           <div className="book-shelf-changer">
-            <select>
+            <select value={this.state.value} onChange={this.handleChange}>
               <option value="move" disabled>Move to...</option>
-              <option id='currentlyReading' value="currentlyReading">Currently Reading</option>
-              <option id='wantToRead' value="wantToRead">Want to Read</option>
-              <option id='read' value="read">Read</option>
-              {/* <option id='' value="none">None</option> */}
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option id='' value="none">None</option>
             </select>
           </div>
         </div>
