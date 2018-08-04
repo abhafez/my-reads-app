ee75f9298be8bd80af14def76e7650f305839571
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2018-08-04 23:29:13 +0200

    query updates and search results fetches

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,26 +1,48 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
+import Book from './Book'
 
 class SearchBooks extends React.Component {
-  state = {
-    query: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      searchResults: []
+    }
+    this.updateQuery = this.updateQuery.bind(this);
   }
 
   updateQuery = (query) => {
     this.setState({ query: query.trim() })
+    BooksAPI.search(this.state.query).then((books) => (
+      this.setState({ searchResults: books })
+    ))
   }
 
   render() {
+
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to='/'>Close</Link>
-          <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" value={this.state.query}/>
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link className='close-search' to='/'>Close</Link>
+          <div className='search-books-input-wrapper'>
+            <input
+              type='text'
+              placeholder='Search by title or author'
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+        <div className='search-books-results'>
+          <ol className='books-grid'>
+            {/* {foundBooks.map((book) => (
+              <li draggable="true" key={book.id}>
+                <Book bookDetails={book} />
+              </li>
+            ))} */}
+          </ol>
         </div>
       </div>
     );
